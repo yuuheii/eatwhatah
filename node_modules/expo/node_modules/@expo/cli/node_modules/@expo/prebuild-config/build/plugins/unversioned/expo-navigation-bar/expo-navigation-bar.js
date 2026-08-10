21adcc68c68ea45dddacfc8805b8e833ec9c7531
yuuheii <yuuheii310@gmail.com>
2026-08-10 04:55:38 +0000

    Save changes

---
+++ node_modules/expo/node_modules/@expo/cli/node_modules/@expo/prebuild-config/build/plugins/unversioned/expo-navigation-bar/expo-navigation-bar.js
@@ -0,0 +1,25 @@
+"use strict";
+
+exports.__esModule = true;
+exports.default = void 0;
+function _withAndroidNavigationBar() {
+  const data = require("./withAndroidNavigationBar");
+  _withAndroidNavigationBar = function () {
+    return data;
+  };
+  return data;
+}
+function _createLegacyPlugin() {
+  const data = require("../createLegacyPlugin");
+  _createLegacyPlugin = function () {
+    return data;
+  };
+  return data;
+}
+var _default = exports.default = (0, _createLegacyPlugin().createLegacyPlugin)({
+  packageName: 'expo-navigation-bar',
+  fallback: [
+  // Android
+  _withAndroidNavigationBar().withNavigationBar]
+});
+//# sourceMappingURL=expo-navigation-bar.js.map
